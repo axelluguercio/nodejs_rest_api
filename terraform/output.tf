4e042111a57008ed1b1f0dd57aac5fa8fc7828c8
Axel Luguercio <axelluguercio@gmail.com>
2022-09-17 11:59:45 -0300

    uploading network and environment files

---
+++ terraform/output.tf
@@ -0,0 +1,11 @@
+output "connect_to_zonal_cluster" {
+  value = var.enable_private_endpoint ? "gcloud container clusters get-credentials ${var.gke_cluster_name} --zone ${var.zone} --project ${var.project_id} --internal-ip" : "gcloud container clusters get-credentials ${var.gke_cluster_name} --zone ${var.zone} --project ${var.project_id}"
+}
+
+output "enable_workload_identity_on_node_pool" {
+  value = var.workload_metadata_enabled ? "N/A - Workload Identity already enabled" : "gcloud container node-pools update ${var.gke_nodepool_name} --cluster ${var.gke_cluster_name} --workload-metadata=GKE_METADATA --zone ${var.zone} --project ${var.project_id}"
+}
+
+output "workload_identity_service_account" {
+  value = google_service_account.wi_gsa.email
+}
