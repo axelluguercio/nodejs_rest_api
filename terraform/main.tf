985528a66e421a59ad5f5df3cca13674e9c22498
Axel Luguercio <axelluguercio@gmail.com>
2022-09-17 17:43:19 -0300

    fix project id

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 # bucket to store backend service for terraform
 terraform {
   backend "gcs" {
-    bucket = "devops-test-rest-api-terraform"
+    bucket = "devops-test-362819-terraform"
     prefix = "/state/devops-test"
   }
 }
